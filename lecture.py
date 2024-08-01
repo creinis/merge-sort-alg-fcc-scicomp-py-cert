c7cba2e89ffa844be47bd78406d3d5d3a8bf2302
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 17:05:45 +0200

    feat: pass array as argument in merge_sort function

--- lecture.py
+++ lecture.py
@@ -15,5 +15,13 @@
 def merge_sort():
     pass
 
+# Step 2
+
+# You'll need a parameter that denotes the data collection to be sorted. 
+# Create a parameter called array in the merge_sort function.
+
+def merge_sort(array):
+    pass
+
 
 
