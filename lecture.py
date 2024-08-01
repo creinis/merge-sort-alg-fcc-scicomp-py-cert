1da6c78949ef36b6bbec386c069c88f63362d926
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 17:15:30 +0200

    feat: if condition is true: left_part[left_array_index] to array[sorted_index]

--- lecture.py
+++ lecture.py
@@ -130,4 +130,16 @@ def merge_sort(array):
         if left_part[left_array_index] < right_part[right_array_index]:
             pass
 
+# Step 13
+
+# When the if condition evaluates to True, it means that the element in the left_part list is smaller than 
+# the element it is being compared to in the right_part list.
+
+# In that case, you can assign the left_part index to the sorted array.
+
+# Inside the if block, remove pass and assign left_part[left_array_index] to array[sorted_index].
+
+    while left_array_index < len(left_part) and right_array_index < len(right_part):
+        if left_part[left_array_index] < right_part[right_array_index]:
+            array[sorted_index] = left_part[left_array_index]
 
