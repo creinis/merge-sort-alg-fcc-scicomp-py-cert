6c72af715f64742637b209370e1d96f83c796db7
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 17:09:26 +0200

    feat: keep dividing each list until every element stands alone in its own list. A list with a single number is always sorted. To do that, recursively call merge_sort inside your function

--- lecture.py
+++ lecture.py
@@ -62,6 +62,24 @@ def merge_sort(array):
     left_part = array[:middle_point]
     right_part = array[middle_point:]
 
+# Step 6
+
+# Now that you've divided the array list into two separate lists, you'll keep dividing each list until every 
+# element stands alone in its own list. 
+# A list with a single number is always sorted.
+
+# To do that, recursively call merge_sort inside your function.
+
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+
+    middle_point = len(array) // 2
+    left_part = array[:middle_point]
+    right_part = array[middle_point:]
+    merge_sort()
+
+
 
 
 
