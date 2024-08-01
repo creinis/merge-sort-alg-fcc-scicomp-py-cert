79834573f78cdaaa1253b7871732640866c962ae
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 17:07:50 +0200

    feat: divide array into two parts; 1st part: Use the slice syntax to extract the left half of array and assign it to a variable named left_part

--- lecture.py
+++ lecture.py
@@ -42,3 +42,17 @@ def merge_sort(array):
 def merge_sort(array):
     middle_point = len(array) // 2
 
+# Step 4
+
+# In the previous step, you got the mid point. 
+# You can use it to divide array into two and assign each part to new variables.
+
+# Use the slice syntax to extract the left half of array and assign it to a variable named left_part.
+
+def merge_sort(array):
+    middle_point = len(array) // 2
+    left_part = array[:middle_point]
+
+
+
+
