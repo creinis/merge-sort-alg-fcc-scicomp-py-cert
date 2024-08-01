46ef4d1af2d387f582a91321fbbc539948c57972
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 17:11:06 +0200

    feat: Create three variables: left_array_index, right_array_index, and sorted_index and set their values to 0

--- lecture.py
+++ lecture.py
@@ -94,7 +94,19 @@ def merge_sort(array):
     merge_sort(left_part)
     merge_sort(right_part)
 
+# Step 10
 
+# Now it's time to sort and merge the lists (left_part and right_part) into the original array.
+
+# You can do this by comparing elements on both lists, and merging the smaller element to the main list. 
+# You are going to do this comparison for all the indexes in left_part and right_part.
+
+# Create three variables: left_array_index, right_array_index, and sorted_index and set their values to 0. 
+# These variables will help you keep track of each index during the sorting process.
+
+    left_array_index = 0
+    right_array_index = 0
+    sorted_index = 0
 
 
 
