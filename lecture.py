2307c20792dc0c8b009f32ba6c7eb1a663bac195
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 17:08:20 +0200

    feat: 2nd part os array variable sliced

--- lecture.py
+++ lecture.py
@@ -53,6 +53,15 @@ def merge_sort(array):
     middle_point = len(array) // 2
     left_part = array[:middle_point]
 
+# Step 5
+
+# Use the slice syntax to extract the right half of array and assign it to a variable named right_part.
+
+def merge_sort(array):
+    middle_point = len(array) // 2
+    left_part = array[:middle_point]
+    right_part = array[middle_point:]
+
 
 
 
