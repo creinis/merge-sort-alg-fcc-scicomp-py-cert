8e86d87b2688cf3d360f6c117dc041bdfb43f002
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 17:11:54 +0200

    feat: while loop that compares an element in left_part to an element in right_part,  and merges the smaller element to the main array list. Create two conditions for the loop: one that checks whether the left_array_index is less than the length of # left_part and another condition that checks whether right_array_index is less than the length of right_part

--- lecture.py
+++ lecture.py
@@ -108,5 +108,17 @@ def merge_sort(array):
     right_array_index = 0
     sorted_index = 0
 
+# Step 11
+
+# Inside your function, create a while loop that compares an element in left_part to an element in right_part, 
+# and merges the smaller element to the main array list.
+
+# Create two conditions for the loop: one that checks whether the left_array_index is less than the length of 
+# left_part and another condition that checks whether right_array_index is less than the length of right_part.
+
+    while left_array_index < len(left_part) and right_array_index < len(right_part):
+        pass
+
+
 
 
